8ae895928491201e231d3d3d05cb3bc1bebcffbc
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-09-24 12:58:09 +0300

    Create fscanf_17.c

---
+++ fscanf_17.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+	FILE *f;
+	int ival, retval;
+	char buffer[200];
+
+	if ((f = fopen("asal1000.txt", "r")) == NULL) {
+		fprintf(stderr, "dosya acilamadi\n");
+		return 1;
+	}
+
+	while ((retval = fscanf(f, "%d", &ival)) != EOF) {
+		if (retval == 0) {
+			fscanf(f, "%s", buffer);
+			printf("\n[%s] gecerli bir tam sayi degil\n", buffer);
+		}
+		else {
+			printf("%d ", ival);
+		}
+	}
+
+	fclose(f);
+}
