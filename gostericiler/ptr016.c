6d49f15c83650cf8199679720fd28bf2ea5c8540
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-10-09 12:14:39 +0300

    Create ptr016.c

---
+++ gostericiler/ptr016.c
@@ -0,0 +1,14 @@
+// adres operatörünün operandı sol taraf değeri ifadesi (L value expression) olmak zorunda
+
+int main()
+{
+	int x = 10;
+	double dval = 2.3;
+
+	//&10    gecersiz
+	//&(x + 5)   gecersiz
+  //&+x   gecersiz
+	&x;   //gecerli
+	&dval;   //gecerli
+  &(x);   //gecerli
+}
