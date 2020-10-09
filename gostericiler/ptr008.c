8d6672968991340fb7ce84a567841f7ce27fac19
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-10-09 13:49:06 +0300

    Create ptr008.c

---
+++ gostericiler/ptr008.c
@@ -0,0 +1,14 @@
+int main()
+{
+	int x = 10;
+	int y = 40;
+
+	int* p = &x;  //initialization
+	//p nin değeri x'in adresi
+	//p x'i gösteriyor
+	//p points to x
+
+	p = &y;
+	//p nin değeri y'nin adresi
+	//p points to y
+}
