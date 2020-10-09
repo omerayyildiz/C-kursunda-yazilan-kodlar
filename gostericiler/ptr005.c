c47bd5194971b8fef64a0c8cd9215408744ad15b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-10-09 12:07:57 +0300

    Create ptr005.c

---
+++ gostericiler/ptr005.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	char*   cp;
+	int*		ip;
+	double* dp;
+
+	printf("sizeof(cp) = %zu\n", sizeof(cp));
+	printf("sizeof(ip) = %zu\n", sizeof(ip));
+	printf("sizeof(dp) = %zu\n", sizeof(dp));
+}
