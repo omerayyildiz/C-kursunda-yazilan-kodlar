c4f4bd060b03c770965f3c8d93dcbce6d4fedcbc
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-10-09 11:53:17 +0300

    Update and rename gostericler/ptr016.c to gostericiler/ptor003.c

---
+++ gostericiler/ptor003.c
@@ -0,0 +1,24 @@
+/*
+   Virgüllerle ayrılmış (comma separated) bildirimlerde asterisk atomu yalnızca önüne geldiği ismi niteliyor.
+*/
+
+void f1()
+{
+	int *p1, p2, p3;
+	//int* p1;
+	//int p2;
+	// int p3;
+  ///...
+
+}
+
+
+void f1()
+{
+	int *p1, *p2, *p3;
+	// int* p1;
+	// int* p2;
+	// int* p3;
+  ///...
+
+}
