94a261e3e5de582c192d44fbde2bdc123d6c6b52
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-10-10 08:25:50 +0300

    Create ptr026.c

---
+++ gostericiler/ptr026.c
@@ -0,0 +1,11 @@
+
+//içerik (dereferencing) operatörünün operandı adres olmak zorunda
+
+int main()
+{
+	int x = 10;
+
+	*x; //gecersiz x ifadesi adres degil
+	*12; //gecersiz 12 ifadesi adres degil
+
+}
