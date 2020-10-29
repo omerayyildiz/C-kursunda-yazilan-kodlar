fde2b9650b75b27cdb440e8f009aeebe804eeba9
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-10-29 14:39:15 +0300

    Create puts_01.c

---
+++ diziler/yazilar/puts_01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	char str[20] = "sumeyye";
+	int x = 345;
+	/*
+		for (int i = 0; str[i] != '\0'; ++i)
+			putchar(str[i]);
+
+		putchar('\n');
+	*/
+
+	puts(str);
+	printf("x = %d\n", x);
+}
