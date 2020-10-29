81f362d321eb3f0b8e20dde1ad88d84bd373b8cd
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-10-29 14:05:16 +0300

    Create yazi05.c

---
+++ diziler/yazilar/yazi05.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+#define			SIZE		40
+
+int main()
+{
+	char name[SIZE];
+
+	name[0] = 'A';
+	name[1] = 'H';
+	name[2] = 'M';
+	name[3] = 'E';
+	name[4] = 'T';
+
+	//yazinin sonunda null karakter yok
+
+	for (int i = 0; name[i] != '\0'; ++i)  //tanimsiz davranis
+		putchar(name[i]);
+
+	putchar('\n');
+}
