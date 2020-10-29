090cfa1aec14905df58baf64d4e2081bf9f613ff
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-10-29 16:51:56 +0300

    Create printf_01.c

---
+++ diziler/yazilar/printf_01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	char town[20] = "yozgat";
+	char name[20] = "sumeyye";
+	char surname[20] = "akkaya";
+	int age = 45;
+
+	printf("%s ilinde ikamet eden %d yasindaki %s %s evlendi\n", town, age, name, surname);
+}
