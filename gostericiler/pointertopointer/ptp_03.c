b3cf3813b89b8850addc014eb6a829e80dd32cb1
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2020-11-01 08:32:48 +0300

    Create ptp_03.c

---
+++ gostericiler/pointertopointer/ptp_03.c
@@ -0,0 +1,20 @@
+int main()
+{
+	int x = 10;
+	int *p = &x;
+	int **ptr = &p;
+	//...
+}
+
+
+ifade				data type				value category
+=========           ==========              ==============
+  x						int							L
+  &x					int *						R
+  p						int *						L
+  *p					int							L
+  &p					int**						R
+  ptr					int**						L
+  *ptr					int*						L
+  **ptr					int							L
+  &ptr					int***						R
